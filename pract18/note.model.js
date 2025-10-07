189528240ba260e14f23d526070844c7e9d1d8b6
Sam <146644053+SamOp-04@users.noreply.github.com>
2025-10-07 16:11:50 +0530

    Delete pract18 directory

--- pract18/note.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const noteSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now }
-});
-
-export default mongoose.model('Note', noteSchema);
